3d46644c6e3456b57363129dc4cd5e0345336829
Kelgors <github.hurt819@passmail.net>
2026-05-04 19:59:04 +0200

    Add Microsoft repository and install Visual Studio Code

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,10 @@
 
 set -ouex pipefail
 
+# Add Microsoft Repo for Visual Studio Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,7 +14,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y code
 
 # Use a COPR Example:
 #
